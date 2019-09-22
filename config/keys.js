769a65f75d5854ee6da457fdae4684176ba441ca
Randy Reyes <rqreyes@gmail.com>
2019-09-21 19:08:38 -0700

    add key

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Client_ID: 'h0TJ5eDrW_ewGKxPz6rnWw',
+  API_KEY:
+    'MfDMUex5QqBEmikkv_loFMylwePCLv5V68l3okBXaqK0wjTkwcIlackwCcAsUOg0Gf2c3KEa59wz1RyiV1CFiMF_lkxc268shpIysJXuZhVwEN1nDyTWEci4q1-GXXYx'
+};

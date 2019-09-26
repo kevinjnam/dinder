b27c4b3f1cc10b3a8b2329e91ef7e59eb8590c80
Darren Napier <dnaps@Darrens-MacBook-Pro.local>
2019-09-26 10:22:09 -0700

    Added back in the original database

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 let url =
-	"postgres://kgcddhnh:KavuQz1fDvPlhVJdFwZbiLQMkMm2n_tY@salt.db.elephantsql.com:5432/kgcddhnh";
+	"postgres://hddufohk:1ur6fgrvf7bVO_oN61Qbd-xr5gBuk_mi@salt.db.elephantsql.com:5432/hddufohk";
 const pool = new Pool({
 	connectionString: url
 });
